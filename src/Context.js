49c0792d6b2971ffe0ef6e597461f3cdda275519
Sandi Sopian <shunasand73@gmail.com>
2022-02-14 19:56:27 +0700

    Detail Products

--- src/Context.js
+++ src/Context.js
@@ -5,18 +5,66 @@ const ProductContext = React.createContext(); //context berguna untuk salah satu
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct,
+    cart: [],
+    modalOpen: true,
+    modelProduct: detailProduct,
   };
-  handleDetail = () => {
-    console.log("hello detail");
+
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item }; // ... sama seperti * untuk menangkap semua value
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
-  addToCart = () => {
-    console.log("hello cart");
+
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id); //untuk mencari id dari product id diatas
+    return product;
+  };
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+  addToCart = (id) => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] };
+    });
+  };
+
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+  closeModal = (id) => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
   };
 
   render() {
-    return <ProductContext.Provider value={{ ...this.state, handleDetail: this.handleDetail, addToCart: this.addToCart }}>{this.props.children}</ProductContext.Provider>;
+    return <ProductContext.Provider value={{ ...this.state, handleDetail: this.handleDetail, addToCart: this.addToCart, openModal: this.openModal, closeModal: this.closeModal }}>{this.props.children}</ProductContext.Provider>;
   } // ... berguna untuk mengambil semua state
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
 import styled from "styled-components";
+import { ButtonContainer } from "./Button";
+import logo from "../logo.svg";
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -25,20 +26,18 @@ class Navbar extends Component {
             My Cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
 export default Navbar;
 
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  color: var(--mainWhite);
-  &:hover {
-    background: var(--lightBlue);
-    color: car(--mainBlue);
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
   }
 `;
