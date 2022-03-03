36864f246a0c749f1b4a3c0af38c435f3b765436
Sandi Sopian <shunasand73@gmail.com>
2022-03-03 13:23:22 +0700

    Cart Page part 3

--- src/Context.js
+++ src/Context.js
@@ -49,9 +49,14 @@ class ProductProvider extends Component {
     product.count = 1;
     const price = product.price;
     product.total = price;
-    this.setState(() => {
-      return { products: tempProducts, cart: [...this.state.cart, product] };
-    });
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   openModal = (id) => {
@@ -66,16 +71,99 @@ class ProductProvider extends Component {
     });
   };
   increase = (id) => {
-    console.log("this increase");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find((item) => item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+
+    this.setState(
+      () => {
+        return;
+        {
+          cart: [...tempCart];
+        }
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
   decrease = (id) => {
-    console.log("decrase qty");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find((item) => item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count - 1;
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = product.count * product.price;
+      this.setState(
+        () => {
+          return;
+          {
+            cart: [...tempCart];
+          }
+        },
+        () => {
+          this.addTotals();
+        }
+      );
+    }
   };
   removeItem = (id) => {
-    console.log("item removed");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter((item) => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removeProduct = tempProducts[index];
+    removeProduct.inCart = false;
+    removeProduct.count = 0;
+    removeProduct.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts],
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
+
   clearCart = () => {
-    console.log("cart cleared");
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
+  };
+  addTotals = () => {
+    let subtotal = 0;
+    this.state.cart.map((item) => (subtotal += item.total));
+    const tempTax = subtotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subtotal + tax;
+    this.setState(() => {
+      return {
+        cartSubtotal: subtotal,
+        cartTax: tax,
+        cartTotal: total,
+      };
+    });
   };
   render() {
     return (
--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -7,12 +7,24 @@ export default function CartColumns({ value }) {
     <React.Fragment>
       <div className="container">
         <div className="row">
-          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-12 text-capitalize text-end">
             <Link to="/">
               <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart}>
                 Clear Cart
               </button>
             </Link>
+            <h5>
+              <span className="text-title">subtotal :</span>
+              <strong>Rp. {cartSubtotal}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">tax :</span>
+              <strong>Rp. {cartTax}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">total :</span>
+              <strong>Rp. {cartTotal}</strong>
+            </h5>
           </div>
         </div>
       </div>
